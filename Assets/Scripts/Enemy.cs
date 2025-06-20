60884bcabc17be805256d27360d9a524b6b588c8
Nikita Astratovs <104262534+Kulakito@users.noreply.github.com>
2025-06-20 20:03:12 +0300

    Сделал реакцию врага на игрока

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour
@@ -11,12 +13,22 @@ public class Enemy : MonoBehaviour
 
     private NavMeshAgent _agent;
 
+    private Transform _player;
+
+    [SerializeField] private float visionRange = 10.0f, visionAngle = 100.0f;
+
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _player = FindFirstObjectByType<PlayerMovement>().transform;
 
         if (_targets != null && _targets.Length > 0) StartCoroutine(Patrolling());
         else Debug.LogError("Нет или недостаточно точек патруля");
+    }
+
+    private void Update()
+    {
+        TryFindPlayer();
     }
 
     private IEnumerator Patrolling()
@@ -27,12 +39,35 @@ public class Enemy : MonoBehaviour
             {
                 _agent.SetDestination(target.position);
 
-                while (_agent.pathPending || _agent.remainingDistance > _agent.stoppingDistance)
+                while (_agent.pathPending || _agent.remainingDistance > _agent.stoppingDistance) // ждём, пока игрок дойдёт до нужной точки и страхуемся, если путь ещё не построен
                 {
                     yield return null;
                 }
 
                 yield return new WaitForSeconds(_pauseTime);
+            }
+        }
+    }
+
+    private void TryFindPlayer()
+    {
+        if (Vector3.Distance(transform.position, _player.position) <= visionRange)
+        {
+            Vector3 directionToTarget = _player.position - transform.position;
+            directionToTarget.y = 0;
+
+            float angleToTarget = Vector3.SignedAngle(directionToTarget, transform.forward, Vector3.up);
+
+            if (MathF.Abs(angleToTarget) <= visionAngle)
+            {
+                GetComponent<Collider>().enabled = false;
+                if (Physics.Raycast(transform.position, _player.position - transform.position, out RaycastHit hit, visionRange) && hit.transform == _player)
+                {
+                    print(1);
+                    _agent.ResetPath();
+                    StopAllCoroutines();
+                }
+                GetComponent<Collider>().enabled = true;
             }
         }
     }
